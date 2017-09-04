36894f9ef7b05a805e75c4ca14ff2cdbd13877ca
Jack Rostron Bink <jr@bink.com>
2017-09-04 16:36:38 +0100

    W2 Exercise 2: Square

---
+++ w2_simple_programs_ex2_square.py
@@ -0,0 +1,13 @@
+'''
+Write a Python function, square, that takes in one number and returns the square of that number.
+
+This function takes in one number and returns one number.
+'''
+
+def square(x):
+	'''
+	x: int or float.
+	'''
+	return x * x
+	
+print("Square of 3 is " + str(square(3)))
